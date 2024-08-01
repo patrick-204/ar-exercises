54d5cc304ca14d7a8766191efc4b369a0ff5f38f
patrick-204 <p.ohara921@gmail.com>
2024-07-31 21:21:10 -0400

    Complete exercise 6 using Ruby Active Records.

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -8,4 +8,38 @@ require_relative './exercise_5'
 puts "Exercise 6"
 puts "----------"
 
-# Your code goes here ...
+class Store < ActiveRecord::Base
+  has_many :employees
+end
+
+class Employee < ActiveRecord::Base
+  belongs_to :store
+end
+
+@store1.employees.create(
+  first_name: "Khurram", 
+  last_name: "Virani", 
+  hourly_rate: 60
+)
+
+@store1.employees.create(
+  first_name: "Sarah", 
+  last_name: "Johnson", 
+  hourly_rate: 50
+)
+
+@store1.employees.create(
+  first_name: "Paul", 
+  last_name: "Smith", 
+  hourly_rate: 55
+)
+
+puts "Employees for Burnaby store:"
+@store1.employees.each do |employee|
+  puts "#{employee.first_name} #{employee.last_name} - Hourly Rate: #{employee.hourly_rate}"
+end
+
+puts "Employees for Richmond store:"
+@store2.employees.each do |employee|
+  puts "#{employee.first_name} #{employee.last_name} - Hourly Rate: #{employee.hourly_rate}"
+end
